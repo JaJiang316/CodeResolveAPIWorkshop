67e0dc94b0b5a21d321e64c1526d559a6fc66023
Jason Jiang <jasonjiang316@gmail.com>
2021-09-26 23:57:28 -0400

    improved time complexity and improved syntax

--- src/Stock.js
+++ src/Stock.js
@@ -17,16 +17,15 @@ const SearchBar = () => (
 );
 
 function Stock() {
-  const [stockChartXValues, setXValue] = useState([]);
-  const [stockChartYValues, setYValue] = useState([]);
   const [jsonData, setData] = useState([]);
   let x = [];
   let close = [];
   let high = [];
   let low = [];
   let open = [];
-  let trace = {};
+
   useEffect(() => {
+    fetchStock();
     function fetchStock() {
       const API_KEY = 'HGJWFG4N8AQ66ICD';
       let StockSymbol = 'IBM';
@@ -44,30 +43,15 @@ function Stock() {
           }
         )
     }
-    fetchStock();
   }, [])
   
   for(var key in jsonData){
-    x.unshift(key);
-    open.unshift(jsonData[key]['1. open']);
-    high.unshift(jsonData[key]['2. high']);
-    low.unshift(jsonData[key]['3. low']);
-    close.unshift(jsonData[key]['4. close']);
+    x.push(key);
+    open.push(jsonData[key]['1. open']);
+    high.push(jsonData[key]['2. high']);
+    low.push(jsonData[key]['3. low']);
+    close.push(jsonData[key]['4. close']);
   }
-
-  trace = {
-    x: x,
-    close: close,
-    decreasing: {line: {color: '#7F7F7F'}}, 
-    high: high,
-    increasing: {line: {color: '#17BECF'}}, 
-    line: {color: 'rgba(31,119,180,1)'}, 
-    low: low,
-    open: open, 
-    type: 'candlestick', 
-    xaxis: 'x', 
-    yaxis: 'y'
-  };
 
   console.log(jsonData);
 
@@ -75,7 +59,6 @@ function Stock() {
       <div>
         <h1>Stock Market</h1>
         <SearchBar />
-        
         <Plot
           data={[
             {
@@ -87,84 +70,10 @@ function Stock() {
               close: close
             }
           ]}
-          layout={{width: 1000, height: 500, title: 'FB'}}
+          layout={{width: 1500, height: 1000, title: 'FB'}}
         />
       </div>
     )
 }
 
 export default Stock;
-
-
-
-// import React from 'react';
-// import Plot from 'react-plotly.js';
-
-// class Stock extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       stockChartXValues: [],
-//       stockChartYValues: []
-//     }
-//   }
-
-//   componentDidMount() {
-//     this.fetchStock();
-//   }
-
-//   fetchStock() {
-//     const pointerToThis = this;
-//     console.log(pointerToThis);
-//     const API_KEY = 'HGJWFG4N8AQ66ICD';
-//     let StockSymbol = 'FB';
-//     let API_Call = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${StockSymbol}&outputsize=compact&apikey=${API_KEY}`;
-//     let stockChartXValuesFunction = [];
-//     let stockChartYValuesFunction = [];
-
-//     fetch(API_Call)
-//       .then(
-//         function(response) {
-//           return response.json();
-//         }
-//       )
-//       .then(
-//         function(data) {
-//           console.log(data);
-
-//           for (var key in data['Time Series (Daily)']) {
-//             stockChartXValuesFunction.push(key);
-//             stockChartYValuesFunction.push(data['Time Series (Daily)'][key]['1. open']);
-//           }
-
-//           // console.log(stockChartXValuesFunction);
-//           pointerToThis.setState({
-//             stockChartXValues: stockChartXValuesFunction,
-//             stockChartYValues: stockChartYValuesFunction
-//           });
-//         }
-//       )
-//   }
-
-//   render() {
-//     return (
-//       <div>
-//         <h1>Stock Market</h1>
-//         <Plot
-//           data={[
-//             {
-//               x: this.state.stockChartXValues,
-//               y: this.state.stockChartYValues,
-//               type: 'scatter',
-//               mode: 'lines+markers',
-//               marker: {color: 'red'},
-//             }
-//           ]}
-//           layout={{width: 720, height: 440, title: 'A Fancy Plot'}}
-//         />
-//       </div>
-//     )
-//   }
-// }
-
-// export default Stock;
